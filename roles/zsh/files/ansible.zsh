530aa378bef4049d3fa90019949447aa11d02d4a
Ruan C Arcega <ruan.arcega@stone.com.br>
2019-01-09 00:09:49 -0200

    feat: add path of vault password file

--- roles/zsh/files/ansible.zsh
+++ roles/zsh/files/ansible.zsh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-alias avv="ansible-vault view "
-alias avc="ansible-vault create "
-alias ave="ansible-vault edit "
+alias avv="ansible-vault --vault-password-file=${HOME}/.vault view "
+alias avc="ansible-vault --vault-password-file=${HOME}/.vault create "
+alias ave="ansible-vault --vault-password-file=${HOME}/.vault edit "
